1fb40e27d3e0dce5e23052283105f6c5620da5df
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2025-11-28 20:00:43 +0530

    demo

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 module "vpc" {
-  source      = "../terraform-aws-instance"
-  project     = "Roboshop"
-  environment = "dev"
+  source             = "../terraform-aws-instance"
+  project            = "Roboshop"
+  environment        = "dev"
+  public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
+
 
 
 }
