9920fabfdf035953554fdbb048941840ff6cca75
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2025-12-04 17:09:10 +0530

    Updated main.tf and added variables.tf

--- provider.tf
+++ provider.tf
@@ -1,34 +1,23 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.98.0"
-#     }
-#   }
-
-#   backend "s3" {
-#     bucket       = "nag-remote-state"
-#     key          = "workspace-demo/terraform.tfstate"
-#     region       = "us-east-1"
-#     encrypt      = true
-#     use_lockfile = true
-#   }
-# }
-
-# provider "aws" {
-#   region = "us-east-1"
-# }
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.98.0" # Only tell which provider & version to use
+      version = "~> 5.98.0"
     }
+  }
+
+  backend "s3" {
+    bucket       = "nag-remote-state"
+    key          = "workspace-demo"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
 
-# Optional: you can even remove this provider block entirely and let root define it
-provider "aws" { # No region here; region will come from root module
+provider "aws" {
+  region = "us-east-1"
 }
+
+
 
