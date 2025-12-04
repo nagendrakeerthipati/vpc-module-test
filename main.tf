fdf6eea302ac427b8705d9a2b646491ce5310956
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2025-12-04 17:16:24 +0530

    Updated main.tf and added variables.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,1 @@
-module "vpc" {
-  #source = "../terraform-aws-instance"
-  source = "git::https://github.com/nagendrakeerthipati/vpc-module-test.git?ref=main"
 
-  # project            = "Roboshop"
-  # environment        = "dev"
-  # public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24"] 
-
-  project              = var.project
-  environment          = var.environment
-  public_subnet_cidr   = var.public_subnet_cidr
-  private_subnet_cidr  = var.private_subnet_cidr
-  database_subnet_cidr = var.database_subnet_cidr
-
-
-  is_peering_required = true
-
-}
