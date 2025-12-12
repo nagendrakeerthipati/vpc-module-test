b4811bb6c99783d8f2683fb29b4901688ca95ba5
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2025-12-12 22:13:48 +0530

    Updated main.tf and added variables.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
 module "vpc" {
-  #source = "../terraform-aws-vpc"
   source = "git::https://github.com/nagendrakeerthipati/terraform-aws-instance.git?ref=main"
 
-  /* project = "roboshop"
-    environment = "dev"
-    public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"] */
-  project               = var.project
-  environment           = var.environment
+  project     = var.project
+  environment = var.environment
+
   public_subnet_cidrs   = var.public_subnet_cidrs
   private_subnet_cidrs  = var.private_subnet_cidrs
   database_subnet_cidrs = var.database_subnet_cidrs
