0a19ad329be5000a12977ab5ca23db0cc978a88d
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2025-12-12 20:59:52 +0530

    Updated main.tf and added variables.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 module "vpc" {
-  source      = "git::https://github.com/nagendrakeerthipati/terraform-aws-instance.git?ref=main"
+  source      = "git::https://github.com/nagendrakeerthipati/terraform-aws-vpc.git?ref=main"
   project     = var.project
   environment = var.environment
 
-  public_subnet_cidr   = var.public_subnet_cidrs
-  private_subnet_cidr  = var.private_subnet_cidrs
-  database_subnet_cidr = var.database_subnet_cidrs
+  # map the module's plural inputs to your root singular variables
+  public_subnet_cidrs   = var.public_subnet_cidr
+  private_subnet_cidrs  = var.private_subnet_cidr
+  database_subnet_cidrs = var.database_subnet_cidr
 
   is_peering_required = true
 }
