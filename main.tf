cb68b4eaee97ab29bd0d353b977d36cbdff4c2d1
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2025-12-13 10:10:46 +0530

    Force add ignored terraform file

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 module "vpc" {
-  source = "git::https://github.com/nagendrakeerthipati/terraform-aws-instance.git?ref=main"
+  #source = "git::https://github.com/nagendrakeerthipati/terraform-aws-instance.git?ref=main"
+  source = "../terraform-aws-instance"
 
   project     = var.project
   environment = var.environment
 
-  public_subnet_cidrs   = var.public_subnet_cidrs
-  private_subnet_cidrs  = var.private_subnet_cidrs
-  database_subnet_cidrs = var.database_subnet_cidrs
+  public_subnet_cidr   = var.public_subnet_cidrs
+  private_subnet_cidr  = var.private_subnet_cidrs
+  database_subnet_cidr = var.database_subnet_cidrs
 
   is_peering_required = true
 }
